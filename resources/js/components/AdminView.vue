6e23a4e8038306d358237553982871cbf1c25b3b
judah king <judahk065@gmail.com>
2025-11-17 14:20:57 +0100

    commit

---
+++ resources/js/components/AdminView.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="admin-view">
+    <h1>Admin View</h1>
+    <p>This is a placeholder AdminView component.</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AdminView',
+  data() {
+    return {};
+  }
+};
+</script>
+
+<style scoped>
+.admin-view { padding: 1rem; }
+</style>
---
+++ resources/js/components/LogIn.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="login-view">
+    <h2>Login</h2>
+    <p>Placeholder login component.</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'LogIn',
+  data() {
+    return {};
+  }
+};
+</script>
+
+<style scoped>
+.login-view { padding: 1rem; }
+</style>
